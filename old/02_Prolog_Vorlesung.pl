f405f5cfd80fb05a6252b98de33514c0919476cc
s-voelkl <134174522+s-voelkl@users.noreply.github.com>
2025-01-09 22:27:23 +0100

    Deleted archive directory

--- old/02_Prolog_Vorlesung.pl
+++
@@ -1,5 +0,0 @@
-brother(Person, Bruder) :- parent(Elternteil, Person), parent(Elternteil, Bruder), female(Bruder).
-sister(Person, Schwester) :- parent(Elternteil, Person), parent(Elternteil, Schwester), female(Schwester).
-aunt(Person, Tante) :- parent(Elternteil, Person), sister(Elternteil, Person).
-
-has_siblings(Person) :- sister(Person, Schwester); brother(Person, Bruder). %?
